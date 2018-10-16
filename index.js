1ed46b6382d4ecc7a7e90c73274e6c04879781e6
Alina <stereoalina@gmail.com>
2018-10-16 22:32:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (request, response) => {
+
+	var number = request.query.number
+
+	if (number != undefined && number != '') {
+	
+		var result = Math.pow(number, 2)
+
+		response.render('home', {
+			result: 'Число ' + number + ' в квадрате = ' + result
+		})
+	}
+	else
+		response.render('home', {
+		result: ''
+	})
+})
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000)
