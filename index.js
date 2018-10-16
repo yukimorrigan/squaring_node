2369dcca6a499f801671434bb88136d9b6d64a45
Alina <stereoalina@gmail.com>
2018-10-16 23:40:21 +0300

    first commit

--- index.js
+++ index.js
@@ -8,25 +8,23 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (request, response) => {
-	var number = request.query.number
+    var number = request.query.number
 
-	if (number != undefined && number != '') {	
-		var result = Math.pow(number, 2)
-		response.render('home', {
-			result: 'Число ' + number + ' в квадрате = ' + result
-		})
-	}
-	else {
-		response.render('home', {
-			result: ''
-		})
-	}
-		
+    if (number != undefined && number != '') {    
+        var result = Math.pow(number, 2)
+        response.render('home', {
+            result: 'Число ' + number + ' в квадрате = ' + result
+        })
+    }
+    else {
+        response.render('home', {
+            result: ''
+        })
+    }        
 })
 
 app.get('/', (request, response) => {
-	response.render('home');		
+    response.render('home');        
 })
-
 
 app.listen(3000)
